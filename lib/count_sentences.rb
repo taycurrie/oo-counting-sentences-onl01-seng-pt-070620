7e0141f81e2d55d75d4a1fa2ed9df1be3818232b
Jeffrey currie <taycurrie@gmail.com>
2020-07-24 21:19:08 +0000

    Automatically backed up by Learn

---
+++ lib/count_sentences.rb
@@ -0,0 +1,18 @@
+class String
+
+  def sentence?
+    self.end_with?(".")
+  end
+
+  def question?
+    self.end_with?("?")
+  end
+
+  def exclamation?
+    self.end_with?("!")
+  end
+
+  def count_sentences
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+  end
+end
